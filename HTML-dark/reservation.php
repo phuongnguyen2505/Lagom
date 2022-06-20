071f8730768db11a0baa8805c424cc55d3bb2461
khangnhq <khang.quoc.huynh.nguyen@gmail.com>
2022-06-21 02:05:04 +0700

    Lagom

---
+++ HTML-dark/reservation.php
@@ -0,0 +1,40 @@
+<?php
+$subject = 'You Got Message'; // Subject of your email
+$to = 'info@designesia.com';  //Recipient's E-mail
+$emailTo = $_REQUEST['email'];
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$phone = $_REQUEST['phone'];
+$msg = $_REQUEST['message'];
+$date = $_REQUEST['date'];
+$time = $_REQUEST['time'];
+$person = $_REQUEST['person'];
+
+
+$email_from = $name.'<'.$email.'>';
+
+$headers = "MIME-Version: 1.1";
+$headers .= "Content-type: text/html; charset=iso-8859-1";
+$headers .= "From: ".$name.'<'.$email.'>'."\r\n"; // Sender's E-mail
+$headers .= "Return-Path:"."From:" . $email;
+
+$message .= 'Name : ' . $name . "\n";
+$message .= 'Email : ' . $email . "\n";
+$message .= 'Phone : ' . $phone . "\n";
+$message .= 'Date : ' . $date . "\n";
+$message .= 'Time : ' . $time . "\n";
+$message .= 'Person : ' . $person . "\n";
+$message .= 'Message : ' . $msg;
+
+if (@mail($to, $subject, $message, $email_from))
+{
+	// Transfer the value 'sent' to ajax function for showing success message.
+	echo 'sent';
+}
+else
+{
+	// Transfer the value 'failed' to ajax function for showing error message.
+	echo 'failed';
+}
+?>
